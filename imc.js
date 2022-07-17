704352b788e4ef307859826f2cc93bb77d69bf1f
giovanesouza <developergiovanesouza@gmail.com>
2022-07-16 23:51:34 -0300

    Reestruturação do código em todos os documentos - HTML, CSS e JS -, adição do campo classificaçã

--- imc.js
+++ imc.js
@@ -1,12 +1,29 @@
 
-function calc_imc () {
 
-    let massa = document.getElementById('peso').value; 
-    let estatura = document.getElementById('est').value;
-    let imc = massa / (estatura ** 2);
-    let resultado = document.getElementById('resultado');
 
-    resultado = imc;
-  
+
+function calculoImc() {
+    let peso = document.getElementById("peso").value;
+    let estatura = document.getElementById("est").value;
+    let imc = peso / estatura ** 2;
+
+    let resultado = document.getElementById("resultado").innerHTML = imc.toFixed(2);
+
 }
 
+function classificacaoImc() {
+
+     
+    if (imc < 18.5) {
+        console.log("Baixo peso.").tofixed;
+    
+    } else if (imc >= 18.5 && imc < 25) {
+        console.log("Eutrofia.");
+    
+    } else if (imc >= 25 && imc < 30) {
+        console.log("Sobrepeso.");
+    
+    } else if (imc >= 30) {
+        console.log("Obesidade.");
+    }
+}
